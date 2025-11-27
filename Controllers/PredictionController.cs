3631bdf537f23c524348cd01b46cad1556486b4b
PetrutaH <167861117+PetrutaH@users.noreply.github.com>
2025-11-27 20:21:06 +0200

    laborator 4 finalizat

--- Controllers/PredictionController.cs
+++ Controllers/PredictionController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Hulujan_Iulia_Petruta_lab4M;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.ML;
-using static Hulujan_Iulia_Petruta_lab4M.PricePredictionModel; 
+using  PriceModel=Hulujan_Iulia_Petruta_lab4M.PricePredictionModel; 
+using DurationModel= Hulujan_Iulia_Petruta_lab4M.DurationPredictionModel;
 
 namespace Hulujan_Iulia_Petruta_lab4M.Controllers
 {
@@ -11,19 +13,33 @@ namespace Hulujan_Iulia_Petruta_lab4M.Controllers
         //    return View();
         //}
 
-        public IActionResult Price(ModelInput input)
+        public IActionResult Price(PriceModel.ModelInput input)
         {
             //Load the model
             MLContext mlContext = new MLContext();
             // Create prediction engine related to the loaded train model
             ITransformer mlModel = mlContext.Model.Load(@"..\Hulujan_Iulia_Petruta_lab4M\PricePredictionModel.mlnet", out var modelInputSchema);
-            var predEngine = mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(mlModel);
+            var predEngine = mlContext.Model.CreatePredictionEngine<PriceModel.ModelInput, PriceModel.ModelOutput>(mlModel);
 
             //Try model on sample data to predict fair price
-            ModelOutput result = predEngine.Predict(input);
+            PriceModel.ModelOutput result = predEngine.Predict(input);
 
             ViewBag.Price = result.Score;
             return View(input);
+        }
+        
+        public IActionResult Duration(DurationModel.ModelInput input)
+        {
+            MLContext mlContext = new MLContext();
+
+            ITransformer mlModel = mlContext.Model.Load(@"..\Hulujan_Iulia_Petruta_lab4M\DurationPredictionModel.mlnet", out var modelInputSchema);
+            var predEngine = mlContext.Model.CreatePredictionEngine<DurationModel.ModelInput, DurationModel.ModelOutput>(mlModel);
+
+            DurationModel.ModelOutput result = predEngine.Predict(input);
+
+            ViewBag.Duration = result.Score;
+            return View(input);
+
         }
 
     }
